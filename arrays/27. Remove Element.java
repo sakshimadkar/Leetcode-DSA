5ae25bfea9e8f521fe1ae9da1d8cc67711b9519a
sakshimadkar <sakshimadkar1908@gmail.com>
2026-01-16 12:43:46 +0530

    27. Remove Element.java

---
+++ arrays/27. Remove Element.java
@@ -0,0 +1,21 @@
+//time complexity: O(n)
+//space complexity: O(1)
+//difficulty : easy
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int len=nums.length;
+        int i=0;
+        while(i<len){
+            if(nums[i]==val){
+                nums[i]=nums[len-1];
+                len--;
+            }
+            else{
+                i++;
+            }
+
+        }
+        return len;
+    }
+}
