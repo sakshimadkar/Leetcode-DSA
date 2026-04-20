09d65823600e7929992c68cea2a48efded0119fb
sakshimadkar <sakshimadkar1908@gmail.com>
2026-04-20 22:06:18 +0530

    LC-(2078)

---
+++ arrays/LC-(2078).java
@@ -0,0 +1,22 @@
+//2078. Two Furthest Houses With Different Colors
+
+class Solution {
+    public int maxDistance(int[] A) {
+        int n = A.length;
+        int left = 0, right = 0;
+
+        for (int i = 0; i < n; i++)
+            if (A[i] != A[n - 1]) {
+                left = i;
+                break;
+            }
+
+        for (int i = n - 1; i >= 0; i--)
+            if (A[i] != A[0]) {
+                right = i;
+                break;
+            }
+
+        return Math.max(n - 1 - left, right);
+    }
+}
