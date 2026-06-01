9be1802ad152d6dee1519400d406236f62c29b67
Sakshi Madkar <sakshimadkar1908@gmail.com>
2026-06-01 13:34:05 +0530

    LC-(2144)

---
+++ arrays/LC-(2144).java
@@ -0,0 +1,16 @@
+//2144. Minimum Cost of Buying Candies With Discount
+
+class Solution {
+    public int minimumCost(int[] cost) {
+        Arrays.sort(cost);
+        int total = 0;
+
+        for (int i = cost.length - 1; i >= 0; i -= 3) {
+            total += cost[i];
+            if (i - 1 >= 0)
+                total += cost[i - 1];
+        }
+
+        return total;
+    }
+}
