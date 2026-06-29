690a0ebe61085b82c0464f26a1599f0a74673b59
Sakshi Madkar <sakshimadkar1908@gmail.com>
2026-06-29 15:45:56 +0530

    LC-(1967)

---
+++ LC-(1967).java
@@ -0,0 +1,9 @@
+//1967. Number of Strings That Appear as Substrings in Word
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        for (String s : patterns)
+            if (word.indexOf(s) != -1) count++; // -1 when not found
+        return count;
+    }
+}
