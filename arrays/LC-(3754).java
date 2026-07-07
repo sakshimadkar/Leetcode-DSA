5fa7822f702ddfb6b87ca171969b22941b07bc5b
Sakshi Madkar <sakshimadkar1908@gmail.com>
2026-07-07 19:03:06 +0530

    LC-(3754)

---
+++ arrays/LC-(3754).java
@@ -0,0 +1,13 @@
+//3754. Concatenate Non-Zero Digits and Multiply by Sum I
+
+class Solution {
+    public long sumAndMultiply(int n) {
+        long x = 0, s = 0;
+        for (char c : String.valueOf(n).toCharArray())
+            if (c != '0') {
+                x = x * 10 + c - '0';
+                s += c - '0';
+            }
+        return x * s;
+    }
+}
